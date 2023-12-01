ce5d44cdc461ce67cb57f87360bb51b8950ce5b6
Marlina Lubis <lubismarlina5@gmail.com>
2023-12-01 20:37:57 +0700

    update js

--- login.js
+++ login.js
@@ -2,16 +2,17 @@ import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
-export default function PostSignUp() {
+export default function PostSignIn() {
     let target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/Billblis";
-    let tokenkey = "token";
-    let tokenvalue = "5f2dcd0e6f39ad4515c8397819a04a22bd6ff03d63b0eaa5913c9e93a217c33b";
+    // let tokenkey = "token";
+    // let tokenvalue = "5f2dcd0e6f39ad4515c8397819a04a22bd6ff03d63b0eaa5913c9e93a217c33b";
     let datainjson = {
         "username": getValue("username"),
         "password": getValue("password")
     }
 
-    postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
+    // postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
+    postWithToken(target_url,datainjson,responseData);
 }
 
 function responseData(result) {
@@ -38,3 +39,5 @@ function responseData(result) {
         });
     }
 }
+
+window.PostSignIn = PostSignIn;
